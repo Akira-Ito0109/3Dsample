3fb759e4c09c5b95fdaa6ddfdd73b99f646e2c64
Akira-Ito0109 <syobonnito@gmail.com>
2021-03-25 10:48:07 +0900

    初回コミット。プレイヤーの移動実装

---
+++ Assets/Script/move.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class move : MonoBehaviour
+{
+    public float moveSpeed=1.0f;
+    private float movementInputValueZ;
+    private float movementInputValueX;
+    private Rigidbody rb;
+
+    public void PlayerMove()
+    {
+        movementInputValueZ = Input.GetAxis("Vertical");
+        movementInputValueX = Input.GetAxis("Horizontal");
+        rb.velocity = new Vector3(movementInputValueX, 0, movementInputValueZ)*moveSpeed;
+
+    }
+    
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        PlayerMove();
+    }
+}
